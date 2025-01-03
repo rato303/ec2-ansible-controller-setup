58d8ab783f1ec07c29f6dd3e5ea7820ca66c5a71
Toshiya Kimura <tosikimu@gmail.com>
2025-01-03 10:05:18 +0900

    EC2インスタンス作成後にansible実行用コンテナをpullするように修正

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,9 +1,10 @@
 module "ec2_instance" {
-  source        = "../../modules/ec2_instance"
-  ami           = var.ami
-  instance_type = var.instance_type
-  region = var.region
-  environment   = "prod"
+  source            = "../../modules/ec2_instance"
+  ami               = var.ami
+  instance_type     = var.instance_type
+  region            = var.region
+  environment       = "prod"
   security_group_id = var.security_group_id
-  key_name      = var.key_name
+  key_name          = var.key_name
+  ansible_version   = var.ansible_version
 }
